2e981f1b3570d80140c847b8bc06d8c9cacca0ad
Nirav Chavda <nrvdchavda@gmail.com>
2021-10-01 09:24:43 +0530

    added short onliner to ask admin priviledges

--- run.bat
+++ run.bat
@@ -23,27 +23,7 @@ FOR %%A IN (%*) DO (
 )
 
 if "%ADMIN_MODE%" == "true" (
-    REM  --> Check for permissions
-    >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-    REM --> If error flag set, we do not have admin.
-    if '%errorlevel%' NEQ '0' (
-        echo Requesting administrative privileges...
-        goto UACPrompt
-    ) else ( goto gotAdmin )
-
-    :UACPrompt
-        echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-        set params = %*:"="
-        echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-        "%temp%\getadmin.vbs"
-        del "%temp%\getadmin.vbs"
-        exit /B
-
-    :gotAdmin
-        pushd "%CD%"
-        CD /D "%~dp0"
+    if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 )
 
 npm run start
